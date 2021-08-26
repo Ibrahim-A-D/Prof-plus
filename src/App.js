61728537e315c29e2b98a72f32bcd65a67f8b007
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-08-26 11:42:01 +0100

    All pages done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Footer from "./components/Footer";
 import NavBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import ProfDetails from "./pages/ProfDetail";
+import SearchPage from "./pages/SearchPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
         <Switch>
-          <Route path="/" exact component={Home}/>            
-          <Route path="/cour" exact component={ProfDetails}/>
-       </Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/cour" exact component={ProfDetails} />
+          <Route path="/search" exact component={SearchPage} />
+        </Switch>
         <Footer />
       </div>
     </Router>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function SearchBar() {
     return (
-        <div className="search__bar">
-                <div className="input__bar border-right">
-                    <i className="fas fa-book "></i>
-                    <input placeholder="Matière" />
-                </div>
-                <div className="input__bar border-right">
-                    <i className="fas fa-map-marker-alt "></i>
-                    <input placeholder="Lieu" />
-                </div>
-                <div className="input__bar">
-                    <i className="fas fa-graduation-cap "></i>
-                    <input placeholder="Niveau" />
-                </div>
-                <button >Rechercher</button>
-            </div>
-    )
+      <div className="search__bar px-0">
+        <div className="inputBar ">
+          <i className="fas fa-book "></i>
+          <input placeholder="Matière" />
+        </div>
+        <div className="inputBar ">
+          <i className="fas fa-map-marker-alt "></i>
+          <input placeholder="Lieu" />
+        </div>
+        <div className="inputBar">
+          <i className="fas fa-graduation-cap "></i>
+          <input placeholder="Niveau" />
+        </div>
+        <button>Rechercher</button>
+      </div>
+    );
 }
 
 export default SearchBar
